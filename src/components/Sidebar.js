98467394165b4a08403e1a7c0619c80739673f7c
royweb <xytank@gmail.com>
2021-05-12 14:00:37 -0400

    Add search bar with styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 
 import './Sidebar.css';
 
@@ -22,6 +23,15 @@ function Sidebar() {
                         <MoreVertIcon />
                     </IconButton>
                 </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchIcon />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                
             </div>
         </div>
     )
