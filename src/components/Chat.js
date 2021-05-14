0aab0ea85c892954688a47be4e6f3d68dee24da8
royweb <xytank@gmail.com>
2021-05-14 13:47:43 -0400

    Add a function to send a message

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -9,11 +9,17 @@ import MicIcon from '@material-ui/icons/Mic';
 import './Chat.css';
 
 function Chat() {
+    const [input, setInput] = useState('');
     const [seed, setSeed] = useState('');
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, [])
+
+    const sendMessage = e => {
+        e.preventDefault();
+        console.log(input);
+    }
 
     return (
         <div className='chat'>
@@ -61,8 +67,14 @@ function Chat() {
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input type="text" placeholder="Type a message..." />
-                    <button>Send a message</button>
+                    <input
+                        type="text"
+                        placeholder="Type a message..."
+                        value={input}
+                        onChange={e => setInput(e.target.value)} />
+                    <button onClick={sendMessage}>
+                        Send a message
+                    </button>
                 </form>
                 <MicIcon />
             </div>
