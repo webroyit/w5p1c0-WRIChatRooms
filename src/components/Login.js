e5d7915a1cb284387ed5acacccbb9449cb9a3982
royweb <xytank@gmail.com>
2021-05-19 11:28:37 -0400

    Add Google login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 
 import './Login.css';
+import { auth, provider } from '../firebase';
 
 function Login() {
+    const signIn = () => {
+        auth
+            .signInWithPopup(provider)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                alert(error.message);
+            });
+    }
+
     return (
         <div className="login">
             <div className="login__container">
@@ -12,7 +24,7 @@ function Login() {
                     <h1>Sign in to WRI ChatRoom</h1>
                 </div>
 
-                <Button size="large">
+                <Button onClick={signIn} size="large">
                     Sign In with Google
                 </Button>
             </div>
